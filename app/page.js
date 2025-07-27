cb01aaa3e59861a0487d1d5d9356c3eebca1ed8d
keshav-sasanapuri <keshavsasanapuri@gmail.com>
2025-07-26 23:57:38 -0400

    finished website version1. Need to work on dark mode now

--- app/page.js
+++ app/page.js
@@ -1,31 +1,36 @@
 "use client";
 import {BsFillMoonStarsFill} from "react-icons/bs";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {
     AiFillTwitterCircle,
     AiFillLinkedin,
     AiFillYoutube
 } from "react-icons/ai";
 import Image from "next/image";
-import keshavdp from "../public/keshav-dp-test.jpg"
-import design from "../public/design.png"
-import code from "../public/code.png"
-import consulting from "../public/consulting.png"
+import keshavdp from "../public/keshav-dp-test.jpg";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
 
 export default function Home() {
   const[darkMode, setDarkMode] = useState(false);
-
-    useEffect(() => {
-      if (darkMode) {
-        document.documentElement.classList.add("dark");
-      } else {
-        document.documentElement.classList.remove("dark");
-      }
-    }, [darkMode]);
+    // useEffect(() => {
+    //   if (darkMode) {
+    //     document.documentElement.classList.add("dark");
+    //   } else {
+    //     document.documentElement.classList.remove("dark");
+    //   }
+    // }, [darkMode]);
 
   return (
-    <div>
-      <main className={"bg-white px-10"}>
+    <div className={darkMode ? "dark" : ""}>
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className={" min-h-screen"}>
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons">develoopedbykeshav</h1>
@@ -45,9 +50,13 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium">Keshav Sasanapuri</h2>
-            <h3 className="text-2xl py-2">Programmer and Awesome</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
+              Keshav Sasanapuri
+            </h2>
+            <h3 className="text-2xl py-2 md:text-3xl">
+              Programmer and Awesome
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
                 PhD Imaging Science with MS CS and BS Math and Statistics.
                 What more could you ask for at this point ya know?
             </p>
@@ -57,7 +66,7 @@ export default function Home() {
             <AiFillLinkedin />
             <AiFillYoutube />
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
             <Image src={keshavdp} alt={"Image of Keshav"} fill objectFit="cover" className="object-cover object-[10%_center]"/> //if you want to scale image, add scale-100
           </div>
         </section>
@@ -77,9 +86,9 @@ export default function Home() {
               if I have bad memory or if I am a mastermind XD.
             </p>
           </div>
-          <div>
+          <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={design} width={100} height={100} className="mx-auto"/>
+              <Image src={design} alt={"design image"} width={100} height={100} className="mx-auto"/>
               <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
               <p className="py-2">
                 Replace this with skills suitable to you. This is just
@@ -91,7 +100,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">Figma</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={code} width={100} height={100} className="mx-auto"/>
+              <Image src={code} alt={"code image"} width={100} height={100} className="mx-auto"/>
               <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
               <p className="py-2">
                 Replace this with skills suitable to you. This is just
@@ -103,7 +112,7 @@ export default function Home() {
               <p className="text-gray-800 py-1">Figma</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={consulting} width={100} height={100} className="mx-auto"/>
+              <Image src={consulting} alt={"consulting image"} width={100} height={100} className="mx-auto"/>
               <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
               <p className="py-2">
                 Replace this with skills suitable to you. This is just
@@ -131,6 +140,26 @@ export default function Home() {
               conclusion is that you will never know
               if I have bad memory or if I am a mastermind XD.
             </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <Image src={web1} alt={"web1 image"} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web2} alt={"web2 image"} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web3} alt={"web3 image"} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web4} alt={"web4 image"} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web5} alt={"web5 image"} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web6} alt={"web6 image"} className="rounded-lg object-cover" width={'100%'} height={'100%'} layout="responsive"/>
+            </div>
           </div>
         </section>
       </main>
