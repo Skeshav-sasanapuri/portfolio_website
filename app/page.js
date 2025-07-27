d25f5194a0828d8bd445245421118cca9a09bfda
keshav-sasanapuri <keshavsasanapuri@gmail.com>
2025-07-27 17:26:06 -0400

    add dark mode

--- app/page.js
+++ app/page.js
@@ -20,31 +20,25 @@ import web6 from "../public/web6.png";
 
 export default function Home() {
   const[darkMode, setDarkMode] = useState(false);
-    // useEffect(() => {
-    //   if (darkMode) {
-    //     document.documentElement.classList.add("dark");
-    //   } else {
-    //     document.documentElement.classList.remove("dark");
-    //   }
-    // }, [darkMode]);
-
   return (
     <div className={darkMode ? "dark" : ""}>
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className={" min-h-screen"}>
-          <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl font-burtons">develoopedbykeshav</h1>
+          <nav className="py-5 mb-12 mt-12 flex justify-between rounded-xl">
+            <h1 className="text-xl font-burtons dark:text-gray-300">develoopedbykeshav</h1>
             <ul className="flex items-center">
-              <li>
-                <BsFillMoonStarsFill
-                onClick={() => setDarkMode(!darkMode)}
-                className={"cursor-pointer text-2xl"}/>
-              </li>
+              <div className="shadow-lg p-1 rounded-xl dark:bg-white">
+                <li>
+                  <BsFillMoonStarsFill
+                    onClick={() => setDarkMode(!darkMode)}
+                    className={"cursor-pointer text-2xl"}/>
+                </li>
+              </div>
               <li>
                 <a className=" bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
                    href={"#"}
                 >
-                  Resume
+                Resume
                 </a>
               </li>
             </ul>
